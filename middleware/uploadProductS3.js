92171552c73f2008d84e5fe53bc2104b61d84a3b
Sanjay2052 <sanjaysanju2052@gmail.com>
2025-12-01 14:40:14 +0530

    this nov 2 1

--- middleware/uploadProductS3.js
+++ middleware/uploadProductS3.js
@@ -1,13 +1,14 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const s3 = require("../config/s3");
+const path = require("path");
 
 module.exports = multer({
   storage: multerS3({
     s3,
     bucket: process.env.AWS_BUCKET,
     key: (req, file, cb) => {
-      cb(null, `uploads/products/${Date.now()}-${file.originalname}`);
+      cb(null, `uploads/products/${Date.now()}${path.extname(file.originalname)}`);
     },
   }),
 });
