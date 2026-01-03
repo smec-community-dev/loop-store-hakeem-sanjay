22b4bc70ac2d9e3311b0254a95572ae2fd3c50f0
hakeem4292 <hakeemummar@gmail.com>
2026-01-03 20:22:47 +0530

    oauth setup

--- config/sellerpassport.js
+++ config/sellerpassport.js
@@ -9,7 +9,7 @@ module.exports = function (passport) {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: "https://loopstore.sanjay.mysmeclabs.com/seller/auth/google/callback"
+                callbackURL: "https://loopstore.hakeem.mysmeclabs.com/seller/auth/google/callback"
             },
 
             async (accessToken, refreshToken, profile, done) => {
